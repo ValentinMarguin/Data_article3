3f6eb36f61bb40679953db4f3bc618773cad1a25
ValentinMarguin <119859180+ValentinMarguin@users.noreply.github.com>
2023-11-16 16:36:30 +0100

    Add files via upload

--- script_Figure.m
+++ script_Figure.m
@@ -1,74 +1,83 @@
-%% Figure 4
+%% figure 4
 
-Figure; 
+figure; 
 plot(temps,mean_slip_fault);
 
-%% Figure 5 : without fluid source
+%% figure 5 : without fluid source
 
 % normal 
 
-Figure;
+figure;
 subplot(4,2,1)
 pdeplot(g_coord_crust,edge,g_num0,'xydata',fluid_pressure_crust_0s,'mesh','off','contour','off');
 colormap(jet);
+caxis([-10 5])
 title('Excess pore pressure (MPa)')
 axis equal
 
 subplot(4,2,3)
 pdeplot(g_coord_crust,edge,g_num0,'xydata',fluid_pressure_crust_10s,'mesh','off','contour','off');
 colormap(jet);
+caxis([-10 5])
 title('Excess pore pressure (MPa)')
 axis equal 
 
 subplot(4,2,5)
 pdeplot(g_coord_crust,edge,g_num0,'xydata',fluid_pressure_crust_20s,'mesh','off','contour','off');
 colormap(jet);
+caxis([-10 5])
 title('Excess pore pressure (MPa)')
 axis equal 
 
 subplot(4,2,7)
 pdeplot(g_coord_crust,edge,g_num0,'xydata',fluid_pressure_crust_5yr,'mesh','off','contour','off');
 colormap(jet);
+caxis([-10 5])
 title('Excess pore pressure (MPa)')
 axis equal 
 
 % reverse 
 
-Figure;
+figure;
 subplot(4,2,2)
 pdeplot(g_coord_crust,edge,g_num0,'xydata',fluid_pressure_crust_0s,'mesh','off','contour','off');
 colormap(jet);
+caxis([-10 20])
 title('Excess pore pressure (MPa)')
 axis equal
 
 subplot(4,2,4)
 pdeplot(g_coord_crust,edge,g_num0,'xydata',fluid_pressure_crust_30s,'mesh','off','contour','off');
 colormap(jet);
+caxis([-10 20])
 title('Excess pore pressure (MPa)')
 axis equal 
 
 subplot(4,2,6)
 pdeplot(g_coord_crust,edge,g_num0,'xydata',fluid_pressure_crust_40s,'mesh','off','contour','off');
 colormap(jet);
+caxis([-10 20])
 title('Excess pore pressure (MPa)')
 axis equal 
 
 subplot(4,2,8)
 pdeplot(g_coord_crust,edge,g_num0,'xydata',fluid_pressure_crust_5yr,'mesh','off','contour','off');
 colormap(jet);
+caxis([-10 20])
 title('Excess pore pressure (MPa)')
 axis equal 
 
-%% Figure 6-7: with fluid source
+%% figure 6-7: with fluid source
 
 %Normal
 
-Figure;
+figure;
 subplot(2,2,1)
 pdeplot(g_coord_crust,edge,g_num0,'xydata',fluid_pressure_crust_0s,'mesh','off','contour','off');
 colormap(jet);
 ylim([-20 0])
 xlim([20 40])
+caxis([0 20])
 title('Excess pore pressure (MPa)')
 axis equal
 
@@ -77,6 +86,7 @@ pdeplot(g_coord_crust,edge,g_num0,'xydata',fluid_pressure_crust_10s,'mesh','off'
 colormap(jet);
 ylim([-20 0])
 xlim([20 40])
+caxis([0 20])
 title('Excess pore pressure (MPa)')
 axis equal 
 
@@ -85,6 +95,7 @@ pdeplot(g_coord_crust,edge,g_num0,'xydata',fluid_pressure_crust_20s,'mesh','off'
 colormap(jet);
 ylim([-20 0])
 xlim([20 40])
+caxis([0 20])
 title('Excess pore pressure (MPa)')
 axis equal 
 
@@ -93,34 +104,38 @@ pdeplot(g_coord_crust,edge,g_num0,'xydata',fluid_pressure_crust_5yr,'mesh','off'
 colormap(jet);
 ylim([-20 0])
 xlim([20 40])
+caxis([0 20])
 title('Excess pore pressure (MPa)')
 axis equal 
 
 
 %Reverse
 
-Figure;
+figure;
 subplot(2,2,1)
 pdeplot(g_coord_crust,edge,g_num0,'xydata',fluid_pressure_crust_0s,'mesh','off','contour','off');
 colormap(jet);
 ylim([-20 0])
 xlim([20 60])
+caxis([-10 100])
 title('Excess pore pressure (MPa)')
 axis equal
 
 subplot(2,2,2)
-pdeplot(g_coord_crust,edge,g_num0,'xydata',fluid_pressure_crust_10s,'mesh','off','contour','off');
+pdeplot(g_coord_crust,edge,g_num0,'xydata',fluid_pressure_crust_30s,'mesh','off','contour','off');
 colormap(jet);
 ylim([-20 0])
 xlim([20 60])
+caxis([-10 100])
 title('Excess pore pressure (MPa)')
 axis equal 
 
 subplot(2,2,3)
-pdeplot(g_coord_crust,edge,g_num0,'xydata',fluid_pressure_crust_20s,'mesh','off','contour','off');
+pdeplot(g_coord_crust,edge,g_num0,'xydata',fluid_pressure_crust_40s,'mesh','off','contour','off');
 colormap(jet);
 ylim([-20 0])
 xlim([20 60])
+caxis([-10 100])
 title('Excess pore pressure (MPa)')
 axis equal 
 
@@ -129,49 +144,33 @@ pdeplot(g_coord_crust,edge,g_num0,'xydata',fluid_pressure_crust_5yr,'mesh','off'
 colormap(jet);
 ylim([-20 0])
 xlim([20 60])
+caxis([-10 100])
 title('Excess pore pressure (MPa)')
 axis equal 
 
-%% Figure 8
+%% figure 8
 
-Figure
+figure
 surf(position_12,temps,fluid_pressure_12,'EdgeColor','interp'); view(2);colorbar
 
-%% Figure 9
+%% figure 9
 
-Figure
-plot(position_surface,surface_velocity_z(i,:))
-%i= times after the first eartquake
-
-
-%% Figure10 
+figure
+i= 12 % time index
+hold on; plot(position_surface,surface_velocity_z(i,:))
 
 
-Figure
+
+%% figure10 
+
+j= 36; %position index (distance between 2 point=0.5 m)
+
+figure
 yyaxis left
 plot(temps,surface_displ_z(:,j))
-% j=position numero (distance between 2 point=0.5 m)
 
 yyaxis right
-plot(temps,fluid_pressure12(:,j))
-% j=position numero (distance between 2 point=0.5 m)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+plot(temps,fluid_pressure_12(:,j))
 
 
 
